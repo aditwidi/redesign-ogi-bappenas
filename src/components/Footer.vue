64bf830a230cd7e54850bcb7df7e86fda4298240
Aditya <adit.widi@gmail.com>
2024-06-24 15:57:23 +0700

    add all files

---
+++ src/components/Footer.vue
@@ -0,0 +1,90 @@
+<template>
+  <footer class="bg-neutral-1 text-white py-10 w-full bottom-0">
+    <div class="container mx-auto flex flex-wrap justify-between">
+      <div class="w-full md:w-1/4 mb-6 md:mb-0">
+        <img src="../assets/logo2.svg" alt="Logo" class="md:w-1/2 ml-16 mb-4" />
+        <p class="text-neutral-5/60 ml-16">
+          Open Government Indonesia (OGI) merupakan sebuah gerakan keterbukaan
+          dan partisipasi melalui kolaborasi inovatif.
+        </p>
+        <div class="flex ml-16 mt-16">
+          <a href="#" class="mr-4">
+            <img src="../assets/Facebook.svg" alt="Facebook" class="h-9" />
+          </a>
+          <a href="#" class="mr-4">
+            <img src="../assets/Twitter.svg" alt="Twitter" class="h-9" />
+          </a>
+          <a href="#" class="mr-4">
+            <img src="../assets/Instagram.svg" alt="Instagram" class="h-9" />
+          </a>
+          <a href="#">
+            <img src="../assets/Youtube.svg" alt="YouTube" class="h-9" />
+          </a>
+        </div>
+      </div>
+      <div class="w-full md:w-1/4 mb-6 md:mb-0 pl-11">
+        <h2 class="font-semibold mb-4">Kontak Info</h2>
+        <h2 class="font-semibold">Alamat</h2>
+        <p class="text-neutral-5/60 mb-4">
+          Jl. Taman Suropati No.2 Menteng, Jakarta Pusat 10310
+        </p>
+        <h2 class="font-semibold">Telepon</h2>
+        <p class="text-neutral-5/60 mb-4">(62-21) 3148551 ext . 3504, 3503</p>
+        <h2 class="font-semibold">Email</h2>
+        <p class="text-neutral-5/60">sekretariat.ogi@bappenas.go.id</p>
+      </div>
+      <div class="w-full md:w-1/4 mb-6 md:mb-0">
+        <h2 class="font-semibold mb-4 ml-11">Eksplorasi</h2>
+        <ul class="text-neutral-5/60 ml-11">
+          <li class="mb-2">
+            <a href="/pelajari/perjalanan" class="hover:text-white">Tentang Kami</a>
+          </li>
+          <li class="mb-2">
+            <a href="/pelajari/kegiatan" class="hover:text-white">Kegiatan Kami</a>
+          </li>
+          <li class="mb-2">
+            <a href="/ran/dokumen-dan-publikasi-data" class="hover:text-white">Dokumen Rencana Aksi OGI</a>
+          </li>
+          <li class="mb-2">
+            <a href="/publikasi/ogi-news" class="hover:text-white">OGI News</a>
+          </li>
+          <li class="mb-2"><a href="/publikasi/laporan-dan-kajian" class="hover:text-white">Laporan</a></li>
+        </ul>
+      </div>
+      <div class="w-full md:w-1/4">
+        <h2 class="font-semibold mb-4">Tautan Lainnya</h2>
+        <ul class="text-neutral-5/60">
+          <li class="mb-2">
+            <a href="https://www.bappenas.go.id/" class="hover:text-white">BAPPENAS</a>
+          </li>
+          <li class="mb-2">
+            <a href="https://ogi.bappenas.go.id/ogplocal_semarang" class="hover:text-white">OGP Local - Semarang</a>
+          </li>
+          <li class="mb-2">
+            <a href="https://ogi.bappenas.go.id/ogplocal_kabbanggai" class="hover:text-white">OGP Local - Banggai</a>
+          </li>
+          <li class="mb-2">
+            <a href="https://ogi.bappenas.go.id/ogplocal_ntb" class="hover:text-white">OGP Local - Sumbawa Barat</a>
+          </li>
+          <li class="mb-2">
+            <a href="https://ogi.bappenas.go.id/ogplocal_kabbrebes" class="hover:text-white"
+              >OGP Local - Kabupaten Brebes</a
+            >
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="border-t border-neutral-3 mt-6 pt-4 mx-28">
+      <p class="text-center text-neutral-5/60">
+        Hak Cipta © 2022 Open Government Indonesia
+      </p>
+    </div>
+  </footer>
+</template>
+  
+  <script>
+export default {
+  name: "Footer",
+};
+</script>
+ 
